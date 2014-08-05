ec305ed3c3b2394068c4dc4605c74c4832387d29
Damien ROTH <damien.roth@gmail.com>
2014-08-05 18:21:35 +0200

    generate homepage and base design

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+    # Retourner un titre basé sur la page.
+    def titre
+        base_titre = "Anima"
+        if @titre.nil?
+            base_titre
+        else
+            "#{base_titre} | #{@titre}"
+        end
+    end
 end
